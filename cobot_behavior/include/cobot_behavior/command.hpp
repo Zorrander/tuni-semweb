a7e0cfa1dfe1b1e8ceea064e097e3aca52e77ed3
Alexandre Angleraud <alexandre.angleraud@gmail.com>
2020-05-26 17:00:22 +0300

    Send ros message

--- cobot_behavior/include/cobot_behavior/command.hpp
+++ cobot_behavior/include/cobot_behavior/command.hpp
@@ -4,6 +4,9 @@
 #include <QObject>
 #include <QString>
 
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
 class Command : public QObject
 {
     Q_OBJECT
@@ -11,7 +14,6 @@ class Command : public QObject
 
 public:
     explicit Command(QObject *parent = nullptr);
-
     QString action() ;
     void setAction(const QString &value);
 
@@ -22,7 +24,9 @@ public slots:
     void send(const QString value);
 
 private:
+    std::shared_ptr<rclcpp::Node> node ;
     QString m_action;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher;
 };
 
 #endif // COMMAND
--- cobot_behavior/src/command.cpp
+++ cobot_behavior/src/command.cpp
@@ -1,12 +1,19 @@
 #include <QObject>
 #include <QString>
-#include "include/cobot_behavior/command.hpp"
+
 #include <iostream>
+
+#include "include/cobot_behavior/command.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
 
 
 Command::Command(QObject *parent) :
     QObject(parent)
 {
+    node = rclcpp::Node::make_shared("minimal_publisher");
+    publisher = node->create_publisher<std_msgs::msg::String>("topic", 10);
 }
 
 QString Command::action()
@@ -23,6 +30,11 @@ void Command::setAction(const QString &value)
 }
 
 void Command::send(const QString value){
-    std::cout << "Hello world: " << value.toUtf8().constData();
+    // std::cin.ignore();
+    std_msgs::msg::String message;
+    message.data = value.toUtf8().constData();
+    RCLCPP_INFO(node->get_logger(), "Publishing: '%s'", message.data.c_str());
+    publisher->publish(message);
+    rclcpp::spin_some(node);
 }
 
